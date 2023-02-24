7359cb67115e466b9cd39d0a2a5e28ab2e4d889d
MaxHuzarevich <pinsk.maks@mail.ru>
2023-02-24 15:24:40 +0300

    - navigation, category  - redux-toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { Catalog } from "./components/Catalog/Catalog"
 import { Header } from "./components/Header/Header"
 import { Navigation } from "./components/Navigation/Navigation"
+import {Provider} from "react-redux";
+import {store} from './store/index'
 
 export const App = () => {
 
   return (
-    <>
+    <Provider store={store}>
       <Header/>
       <main>
         <Navigation/>
         <Catalog/>
       </main>
       <footer></footer>
-    </>
+    </Provider>
   )
 }
 
