5a0a872614cf96b369347b5985d792fec96b32f6
MaxHuzarevich <pinsk.maks@mail.ru>
2023-02-25 01:07:41 +0300

    - catalogSlice, productSlice

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import {Provider} from "react-redux";
 import {store} from './store/index'
 
 export const App = () => {
-
   return (
     <Provider store={store}>
       <Header/>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,27 +1,30 @@
 import style from './Navigation.module.css'
-import React from "react";
+import React, {useEffect} from "react";
 import {Container} from '../Container/Container.jsx'
 import classNames from 'classnames';
 import {useDispatch, useSelector} from "react-redux";
-import {changeCategory} from "../../store/category/categorySlice";
+import {categoryRequestAsync, changeCategory} from "../../store/category/categorySlice";
+import {API_URI} from "../../const";
 
 
 export const Navigation = () => {
-
     const {category, activeCategory} = useSelector((state) => state.category)
     const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(categoryRequestAsync('max'))
+    }, [])
 
     return (
         <nav className={style.navigation}>
             <Container className={style.container}>
                 <ul className={style.list}>
                     {category.map((item, index) =>
-                        <li className={style.item}>
+                        <li key={index} className={style.item}>
                             <button
                                 className={classNames(
                                     style.button,
                                     activeCategory === index ? style.button_active : '')}
-                                style={{backgroundImage: `url(${item.image})`}}
+                                style={{backgroundImage: `url(${API_URI}/${item.image})`}}
                                 onClick={() => {
                                     dispatch(changeCategory({indexCategory: index}))
                                 }}
