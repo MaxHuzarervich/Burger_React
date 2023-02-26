36d63763baae983f0c85b096af40c36d0e993607
MaxHuzarevich <pinsk.maks@mail.ru>
2023-02-26 13:55:50 +0300

    - orderSlice

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
 import {configureStore} from "@reduxjs/toolkit";
 import categoryReducer from './category/categorySlice'
 import productReducer from './product/productSlice'
+import orderReducer, {localStorageMiddleware} from './Order/OrderSlice'
 
 export const store = configureStore({
     reducer: {
         category: categoryReducer,
-        product: productReducer
+        product: productReducer,
+        order: orderReducer
+    },
+
+    middleware: getDefaultMiddleware => {
+        return getDefaultMiddleware().concat(localStorageMiddleware)
     }
 })
